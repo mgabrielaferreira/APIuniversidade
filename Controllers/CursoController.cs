9167731a19dffddef7269190fcbe819f554a3c2a
mgabrielaferreira <ferreira.gabriela@escolar.ifrn.edu.br>
2023-09-26 11:58:05 -0300

    mudando código no curso controler

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -5,61 +5,13 @@ using System.Threading.Tasks;
 using apiUniversidade2.Model;
 using Microsoft.AspNetCore.Mvc;
 
-namespace apiUniversidade2.Controllers;
-
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CursoController : ControllerBase
-    {
-      
-        [HttpGet (Name = "cursos")]
-        public  Curso GetCursos()
+      {HttpGet}
+       public ActionResult<IEnurable<Curso>> Get()
         {
+                var cursos = context.Cursos.ToList();
+                if(cursos is null)
+                return NotFound();
 
-        List<Disciplina> disciplinas = new List<Disciplina>();
-
-        disciplinas.Add(new Disciplina {
-                Nome= "Banco de dados",
-                CargaHoraria= 60,
-                Semestre= 4,
-        });
-
-        disciplinas.Add(new Disciplina{
-                Nome= "Banco de dados",
-                CargaHoraria= 60,
-                Semestre= 4,
-        });
-
-        disciplinas.Add(new Disciplina {
-                Nome= "Banco de dados",
-                CargaHoraria= 60,
-                Semestre= 4, 
-        });
-
-        List<Aluno> alunos = new List<Aluno>();
-        alunos.Add(new Aluno {
-                Id= 20201031,
-                Nome= "Morgana",
-                Area= "Informatica",
-                DataDeNascimento= "8/8/1989",
-                CPF= "99999",
-
-
-
-
-        });
-
-
-        Curso c = new Curso{
-                Nome= "Programação para internet",
-                Area= "TI",
-                Duracao= 4,
-                disciplinas= disciplinas,
-        };
-
+                return cursos;
         
-
-
-         return c;
-    }
-}
+        }
