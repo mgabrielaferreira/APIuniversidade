33710d2f7bc1e9e6da9df23c22813f1624144215
mgabrielaferreira <ferreira.gabriela@escolar.ifrn.edu.br>
2023-11-28 11:12:24 -0300

    adicionando migrations de autenticação

--- Migrations/20231128141203_identity-jwt.Designer.cs
+++ Migrations/20231128141203_identity-jwt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ApiUniversidade.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace apiUniversidade.Migrations
 {
     [DbContext(typeof(ApiUniversidadeContext))]
-    partial class ApiUniversidadeContextModelSnapshot : ModelSnapshot
+    [Migration("20231128141203_identity-jwt")]
+    partial class identityjwt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
