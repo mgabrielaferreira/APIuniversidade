d8058a118dcfae7e2e08bc9f76fbe1f13bbf142c
mgabrielaferreira <ferreira.gabriela@escolar.ifrn.edu.br>
2023-12-05 09:44:00 -0300

    adicionando autoriza controller

---
+++ Controllers/AutorizaController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIuniversidade.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutorizaController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager) 
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
