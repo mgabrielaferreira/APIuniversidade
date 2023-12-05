cda7815a6675c02277564cd8509456042f670a4f
mgabrielaferreira <ferreira.gabriela@escolar.ifrn.edu.br>
2023-12-05 09:59:23 -0300

    adicionando usings

--- Controllers/AutorizaController.cs
+++ Controllers/AutorizaController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
-namespace APIuniversidade.Controllers
+namespace ApiUniversidade.Controllers
 {
     [ApiController]
     [Route("[controller]")]
