28bc6e4726fe0c8a9347264d709b9984edf3ae12
mgabrielaferreira <ferreira.gabriela@escolar.ifrn.edu.br>
2023-12-12 09:50:18 -0300

    adicionando validação de token

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using ApiUniversidade.Context;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,16 @@ builder.Services.AddDbContext<ApiUniversidadeContext>(options => options.UseNpgs
 
 builder.Services.AddControllers();
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApiUniversidadeContext>().AddDefaultTokenProviders();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters{
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidAudience = builder.Configuration["TokenConfiguration:Audience"],
+    ValidIssuer = builder.Configuration["TokenConfiguration:Issuer"],
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(
+        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]))
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
